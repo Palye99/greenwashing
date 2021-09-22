b7d4eaccb0875641c447a92d187b5bc4fe5ec8bb
Théo <theo.mouton@axopen.com>
2021-09-22 17:29:20 +0200

    change map

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -15,6 +15,7 @@ import * as L from 'leaflet';
 })
 export class HomeComponent extends DestroyedDirective implements OnInit {
   user: User;
+  mymap: any;
 
   constructor(private authService: AuthService,
               private dockerService: DockerService,
@@ -24,15 +25,53 @@ export class HomeComponent extends DestroyedDirective implements OnInit {
 
   ngOnInit(): void {
     // Déclaration de la carte avec les coordonnées du centre et le niveau de zoom.
-    const myfrugalmap = L.map('frugalmap').setView([50.6311634, 3.0599573], 12);
+    this.mymap = L.map('mapid').setView([45.764043, 4.835659], 13);
 
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-      attribution: 'Frugal Map'
-    }).addTo(myfrugalmap);
+    //mapbox map
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox/streets-v11',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: 'pk.eyJ1IjoicGFseWUiLCJhIjoiY2t0dmlxNmIyMmFvdzMwbXBoenV6MDFrayJ9.IbnJrzNqvuN0zVy5gv2o3Q'
+    }).addTo(this.mymap);
+
+    // openstreet map
+    // L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    //   attribution: 'Frugal Map'
+    // }).addTo(mymap);
+
+    // Cooordonnée du fichier erronée
+    // parcs.features.forEach(x => {
+    //   var polygone = [];
+    //
+    //   x.geometry.coordinates[0].forEach(y => polygone.push(y));
+    //
+    //   var coucou = L.polygon(polygone, {
+    //     color: 'green',
+    //     fillColor: '#43b144',
+    //     fillOpacity: 0.2
+    //   }).addTo(mymap);
+    //
+    //   console.log(coucou);
+    // });
+
+    this.mymap
+      .on('click', this.onMapClick, this);
+
 
     if (this.authService && this.authService.userData) {
       this.user = this.authService.userData;
     }
+  }
+
+
+  onMapClick(e) {
+    L.popup()
+      .setLatLng(e.latlng)
+      .setContent("Coordonnées : " + e.latlng.toString() + `<button class="btn btn-secondary" (click)="userInfo()"><i class="fas fa-user-tag"></i></button>`)
+      .openOn(this.mymap);
   }
 
   signOut() {
